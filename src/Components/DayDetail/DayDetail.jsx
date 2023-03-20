bc941a8b7bcfb53d803ebe6742fcaeb731f97786
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-03-20 12:41:39 -0300

    clean text area

--- src/Components/DayDetail/DayDetail.jsx
+++ src/Components/DayDetail/DayDetail.jsx
@@ -7,51 +7,52 @@ const API = process.env.REACT_APP_API
 
 
 
-const DayDetail = ({getDayById, dayDetail}) => {
+const DayDetail = ({ getDayById, dayDetail }) => {
 
 	const { id } = useParams()
 
 	const [resume, setResume] = useState('');
 
-	const setingResume = (e) =>{
+	const setingResume = (e) => {
 		setResume(e.target.value)
 
 	}
 
 
-	const sendResume = async () =>{
-		const updated = await axios.put(API + '/days/resume',{resume, idDay: dayDetail.id})
+	const sendResume = async () => {
+		const updated = await axios.put(API + '/days/resume', { resume, idDay: dayDetail.id })
 		console.log(updated)
 		setResume('')
 	}
 
-	useEffect(()=>{
+	useEffect(() => {
 		getDayById(id);
-		
-	},[])
+
+	}, [])
 
 
 	return (
-	<div className='container'>
+		<div className='container'>
 
-		<div className="day-resume-container">
+			<div className="day-resume-container">
 
-		<textarea onChange={setingResume} name="resume" id="" cols="30" rows="10"></textarea>
-		<button onClick={sendResume}>Send</button>
+				<textarea value={resume} onChange={setingResume} name="resume" id="" cols="30" rows="10"></textarea>
+				<button onClick={sendResume}>Send</button>
 
+			</div>
+
+			{dayDetail.resume ? dayDetail.resume : null}
+
+
+			<p>{dayDetail.name}</p>
 		</div>
+	)
+};
 
-		{dayDetail.resume ? dayDetail.resume : null}
-
-
-		<p>{dayDetail.name}</p>
-	</div>  
-)};
-
-const mapStateToProps = (state) =>{
+const mapStateToProps = (state) => {
 	return {
 		dayDetail: state.dayDetail
 	}
 }
 
-export default connect (mapStateToProps,{ getDayById })(DayDetail);
+export default connect(mapStateToProps, { getDayById })(DayDetail);
