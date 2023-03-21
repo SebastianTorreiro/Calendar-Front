92cab4df7c7a79db264ddf961d865017c6b992e6
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-03-20 22:35:44 -0300

    switch edit/write

--- src/Components/DayDetail/DayDetail.jsx
+++ src/Components/DayDetail/DayDetail.jsx
@@ -9,55 +9,72 @@ const API = process.env.REACT_APP_API
 
 const DayDetail = ({ getDayById, dayDetail }) => {
 
+	
+	useEffect(() => {
+		getDayById(id);
+	}, [])
+
 	// que se vea el textarea cuando no haya un resume [v]
-	// cuando haya un resume, que se vea abajo escrito y se desabilite el text area []
-	// cuando este desabilitado el text area, el nombre del boton sera edit y no send o write []
-	// que cuando se toque el boton edit se cargue el texto en el text area para ser editado []
-	// y que continue con su funcionamiento normal. []
+	// cuando haya un resume, que se vea abajo escrito y se desabilite el text area [v]
+	// cuando este desabilitado el text area, el nombre del boton sera edit y no send o write [v]
+	// que cuando se toque el boton edit se cargue el texto en el text area para ser editado [v]
+	// y que continue con su funcionamiento normal. [v]
 	// solucionar el send cuando el estado esta vacio [parcialmente]
 
 	const { id } = useParams()
 
 	const [resume, setResume] = useState('');
+	const [boleean, setBoleaan] = useState(true);
 
 	const setingResume = (e) => {
 		setResume(e.target.value)
 
 	}
 
+	const editResume = (e) => {
+		setResume(dayDetail.resume)
+		setBoleaan(false)
+	}
+
 
 	const sendResume = async () => {
-		if(resume){
+		if (resume) {
 			const updated = await axios.put(API + '/days/resume', { resume, idDay: dayDetail.id })
 			console.log(updated)
-			setResume('') 
+			setResume('')
 		}
 	}
 
-	useEffect(() => {
-		getDayById(id);
+	
 
-	}, [])
 
-  
+
 	return (
 		<div className='container'>
 
-			<div className="day-resume-container">
+			{dayDetail.resume && boleean ?
+				<div className="container-white-resume">
+					<div className="detail-resume">
+						{dayDetail.resume}
+					</div>
+					<div className="button-edit-resume" onClick={editResume}>
+						Edit
+					</div>
+				</div> 
+					:
+				<div className="day-resume-container">
 
-				<textarea
-					value={resume}
-					onChange={setingResume}
-					name="resume"
-					id=""
-					cols="30"
-					rows="10"></textarea>
+					<textarea
+						value={resume}
+						onChange={setingResume}
+						name="resume"
+						id=""
+						cols="30"
+						rows="10"></textarea>
 
-				<button onClick={sendResume}>Write</button>
+					<button onClick={sendResume}>Write</button>
 
-			</div>
-
-			{dayDetail.resume && dayDetail.resume}
+				</div>}
 
 
 			<p>{dayDetail.name}</p>
