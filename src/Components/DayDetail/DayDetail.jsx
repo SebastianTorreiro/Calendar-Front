ebf7035a02f962b5377dbdba389523dfa4339f23
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-03-20 22:07:07 -0300

    solucion parcial del button write

--- src/Components/DayDetail/DayDetail.jsx
+++ src/Components/DayDetail/DayDetail.jsx
@@ -9,6 +9,13 @@ const API = process.env.REACT_APP_API
 
 const DayDetail = ({ getDayById, dayDetail }) => {
 
+	// que se vea el textarea cuando no haya un resume [v]
+	// cuando haya un resume, que se vea abajo escrito y se desabilite el text area []
+	// cuando este desabilitado el text area, el nombre del boton sera edit y no send o write []
+	// que cuando se toque el boton edit se cargue el texto en el text area para ser editado []
+	// y que continue con su funcionamiento normal. []
+	// solucionar el send cuando el estado esta vacio []
+
 	const { id } = useParams()
 
 	const [resume, setResume] = useState('');
@@ -20,9 +27,12 @@ const DayDetail = ({ getDayById, dayDetail }) => {
 
 
 	const sendResume = async () => {
-		const updated = await axios.put(API + '/days/resume', { resume, idDay: dayDetail.id })
-		console.log(updated)
-		setResume('')
+		if(resume){
+			const updated = await axios.put(API + '/days/resume', { resume, idDay: dayDetail.id })
+			console.log(updated)
+			setResume('') 
+		}
+		console.log('ashe')
 	}
 
 	useEffect(() => {
@@ -30,18 +40,25 @@ const DayDetail = ({ getDayById, dayDetail }) => {
 
 	}, [])
 
-
+  
 	return (
 		<div className='container'>
 
 			<div className="day-resume-container">
 
-				<textarea value={resume} onChange={setingResume} name="resume" id="" cols="30" rows="10"></textarea>
+				<textarea
+					value={resume}
+					onChange={setingResume}
+					name="resume"
+					id=""
+					cols="30"
+					rows="10"></textarea>
+
 				<button onClick={sendResume}>Send</button>
 
 			</div>
 
-			{dayDetail.resume ? dayDetail.resume : null}
+			{dayDetail.resume && dayDetail.resume}
 
 
 			<p>{dayDetail.name}</p>
