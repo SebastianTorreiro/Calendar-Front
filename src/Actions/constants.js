6d2859d6d2ff315081c5f333231359befd3607e0
Sebastian Torreiro <sebastiantorreiro@gmail.com>
2023-04-01 14:37:18 -0300

    action types add

--- src/Actions/constants.js
+++ src/Actions/constants.js
@@ -1,3 +1,4 @@
 export const GET_ALL_DAYS = "GET_ALL_DAYS"
 export const GET_DAY_BY_ID = "GET_DAY_BY_ID"
-export const FILTER_BY_MONTH = "FILTER_BY_MOUNTH"
+export const SET_LOADING_TRUE = "SET_LOADING_TRUE"
+export const SET_LOADING_FALSE = "SET_LOADING_FALSE"
--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import { GET_ALL_DAYS, GET_DAY_BY_ID, FILTER_BY_MONTH } from "./constants.js";
+import {
+  GET_ALL_DAYS,
+  GET_DAY_BY_ID,
+  FILTER_BY_MONTH,
+  SET_LOADING_FALSE,
+  SET_LOADING_TRUE,
+} from "./constants.js";
 
 const API = process.env.REACT_APP_API;
 
@@ -28,4 +34,15 @@ export function getDayById(id) {
 
 export function filterByMonth(month) {
   return { type: FILTER_BY_MONTH, payload: month };
+}
+
+export function setLoadingTrue() {
+  return { type: SET_LOADING_TRUE };
+}
+export function setLoadingFalse() {
+  return { type: SET_LOADING_FALSE };
+}
+
+export function errorForzado() {
+  return { type: SET_ERROR_TRUE };
 }
